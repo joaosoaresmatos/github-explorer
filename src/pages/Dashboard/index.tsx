a197cd67c218283d23d74cbd29578496f6969381
Joao Matos <joaosoaresmatos28@gmail.com>
2021-01-06 20:56:15 -0300

    first commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <h1>Explore repositórios no Github</h1>;
+};
+
+export default Dashboard;
